fc8d99f589cb6f30355f2e995e24cb1096c5779c
FrequentErvin <ervin.macic@2gimnazija.edu.ba>
2022-07-15 18:15:47 +0200

    Finish form

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 inputArray = document.querySelectorAll("INPUT");
 console.log(inputArray);
-const password = inputArray[4];
-const confirm_password = inputArray[5];
+
 for(let i = 0; i < inputArray.length; i++){
     inputArray[i].addEventListener("keydown", function(e){
         var whichkey = checkKey(e);
@@ -19,4 +18,16 @@ function focusNext(i){
         console.log(inputArray[i+1]);
         inputArray[i+1].focus();
     }
+}
+var check = function() {
+    const password = document.getElementById("password").value;
+    const confirm_password = document.getElementById("confirm_password").value;
+    const message = document.getElementById('message');
+        if (password == confirm_password) {
+        message.style.color = 'green';
+        message.innerHTML = '*Passwords match';
+      } else {
+        message.style.color = 'red';
+        message.innerHTML = '*Passwords do not match';
+      }
 }
